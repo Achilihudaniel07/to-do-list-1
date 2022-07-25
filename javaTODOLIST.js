ef2fb30a8c9a7f13398857ff1c8fb356058fa00b
unknown <achilihudaniel53@gmail.com>
2022-07-25 08:36:29 +0100

    second commit

--- javaTODOLIST.js
+++ javaTODOLIST.js
@@ -1,27 +1,40 @@
-    var inputs = document.getElementById("userinput2");
+  var inputs = document.getElementById("userinput1");
 
-    var uls= document.getElementsByTagName("ul")[0];
+  var uls= document.querySelector
+  ("ul");
 
-    inputs.addEventListener("keypress",function(event){
+  inputs.addEventListener("keypress",function(event){
 
-    var newli= document.createElement("li")
-    var hr1= document.createElement("hr")
-    var checkb = document.createElement("input")
-    checkb.type = "checkbox"
+  var newli= document.createElement("li")
+  var hr1= document.createElement("hr")
+  var checkb = document.createElement("input")
+  checkb.type = "checkbox"
 
-    if (event.key=="Enter")   
-    {
-    if(inputs.value !== ""){
+  if (event.key=="Enter"){ alert("Are you sure you want to add task?")  
+  {
+  if(inputs.value !== ""){
 
-  
-    newli.append(document.createTextNode(inputs.value), checkb,hr1);
+  newli.append(document.createTextNode(inputs.value), checkb,hr1);
 
-    uls.append(newli)
-    inputs.value = ""
-    }
+  uls.append(newli)
+  inputs.value = ""
+  }
 
-    else if (inputs.value !== "null")
-    {alert ("input value")};
-    }
-    });
+  else if (inputs.value !== "null")
+  {alert ("input value")};
+  }
+  }
+  });
+
+
+  var li = document.querySelector("li")
+  var endtask = document.getElementsByTagName("ul")[0];
+  endtask.addEventListener("click", function(ev){
+  if(ev.target.tagName === "LI"){
+  // ev.target.classList.add("cancel")
+  ev.target.classList.add("delete")
+
+  }
+  }
+  )
 
